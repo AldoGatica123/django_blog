acd6329f0d30795f07d51fd407c88e2ec9dd7ca0
Aldo Gatica <aldogatica123@gmail.com>
2019-01-16 14:07:32 -0600

    comment model, view and template added django smtp server added comment count added to detail

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ The project consists in a blog made from the chapters 1, 2 and 3 from the book D
 
 ## Description
 
-Currently the app only has a post model.  
+Currently the app only has a post model with the functionality to send as an email any published post and make comments.  
+
 There is a superuser to manage the site and a *normaluser*.
 
 ### Model structure
@@ -15,6 +16,11 @@ There is a superuser to manage the site and a *normaluser*.
 |-------------|----------|--------|------|----------|----------|----------|--------|
 | VARCHAR 250 | SLUG 250 | USER   | TEXT | DATETIME | DATETIME | DATETIME | STATUS |
 
+#### Comment
+
+| post | name       | email | body | created  | updated  | active |
+|------|------------|-------|------|----------|----------|--------|
+| POST | VARCHAR 80 | EMAIL | TEXT | DATETIME | DATETIME | BOOL   |
 
 ### Routes
 ```text
@@ -22,6 +28,7 @@ There is a superuser to manage the site and a *normaluser*.
 /blog
     /post_list
     /<year>/<month>/<day>/<slug>
+    /<post_id>/share
 
 ```
 
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
@@ -34,6 +36,40 @@ def post_detail(request, year, month, day, post):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"' \
+                .format(cd['name'], cd['email'], post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments:{}' \
+                .format(post.title, post_url, cd['name'], cd['comments'])
+            send_mail(subject, message, cd['email'], [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
